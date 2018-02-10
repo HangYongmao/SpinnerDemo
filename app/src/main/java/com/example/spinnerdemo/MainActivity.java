2ef7faf571f4d4c480506450eef6cec0cc29caa1
HangYongmao <1981217256@qq.com>
2018-02-10 20:01:57 +0800

    实现下拉列表

--- app/src/main/java/com/example/spinnerdemo/MainActivity.java
+++ app/src/main/java/com/example/spinnerdemo/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.spinnerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Spinner spinner;
+    private List<String> list;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        // 1、设置数据源
+        list = new ArrayList<String>();
+        list.add("北京");
+        list.add("上海");
+        list.add("广州");
+        list.add("深圳");
+
+        // 2、新建ArrayAdapter(数组适配器)
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        // 3、adapter设置一个下拉列表样式
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // 4、Spinner加载
+        spinner.setAdapter(adapter);
     }
 }
